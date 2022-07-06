e4b626505ba1d4bb75e12a0c2fb06122085241e0
Lucas Rodrigo Pereira de Souza <lucasrodrigodes@gmail.com>
2022-07-05 22:39:53 -0300

    projeto finalizado

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import authMiddleware from './app/middlewares/auth';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import TaskController from './app/controllers/TaskController';
 
 const routes = new Router();
 
@@ -11,5 +12,10 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.get('/tasks', TaskController.index);
+routes.post('/tasks', TaskController.store);
+routes.put('/tasks/:task_id', TaskController.update);
+routes.delete('/tasks/:task_id', TaskController.delete);
 
 export default routes;
